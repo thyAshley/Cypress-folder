33be6f4a99efe7d0e9120f95ac0229c3ae470160
Ashley <thy.ashley@gmail.com>
2020-11-11 21:33:59 +0800

    then and wrap function

--- ngx-cypress-test/cypress/integration/firstTest.spec.js
+++ ngx-cypress-test/cypress/integration/firstTest.spec.js
@@ -35,4 +35,48 @@ describe("First test suite", () => {
     // most recommended way by Cypress
     cy.get('[data-cy="imputEmail1"]');
   });
+
+  it("second test", () => {
+    cy.visit("/");
+    cy.get(".menu-title").contains(/forms/i).click();
+    cy.get(".menu-title")
+      .contains(/form layout/i)
+      .click();
+    cy.contains('[status="warning"]', /sign in/i);
+    cy.get("#inputEmail3")
+      .parents("form")
+      .find("button")
+      .should("contain", "Sign in")
+      .parents("form")
+      .find("nb-checkbox")
+      .click();
+
+    cy.contains("nb-card", /horizontal form/i).find('[type="email"]');
+  });
+
+  it.only("then and wrap method", () => {
+    cy.visit("/");
+    cy.get(".menu-title").contains(/forms/i).click();
+    cy.get(".menu-title")
+      .contains(/form layout/i)
+      .click();
+
+    cy.contains("nb-card", /using the grid/i).then((gridForm) => {
+      const emailLabel = gridForm.find('[for="inputEmail1"]').text();
+      const passwordLabel = gridForm.find('[for="inputPassword2"]').text();
+      expect(emailLabel).to.equal("Email");
+      expect(passwordLabel).to.equal("Password");
+    });
+
+    cy.contains("nb-card", /basic form/i).then((basicForm) => {
+      const email = basicForm.find('[for="exampleInputEmail1"]').text();
+      const password = basicForm.find('[for="exampleInputPassword1"]').text();
+      expect(email).to.contain("Email");
+      expect(password).to.equal("Password");
+
+      cy.wrap(basicForm)
+        .find('[for="exampleInputEmail1"]')
+        .should("contain", "Email");
+    });
+  });
 });
