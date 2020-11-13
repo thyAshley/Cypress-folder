3823e0731573d0f8f1bbc322ee945f9d3467eb65
Ashley <thy.ashley@gmail.com>
2020-11-13 21:44:51 +0800

    added test for automation store

---
+++ cypress-folder/cypress/integration/automationstore/contact.spec.ts
@@ -0,0 +1,16 @@
+/// <reference types="cypress">
+
+describe('Test contact on Automation store ', () => {
+  beforeEach(() => {
+    cy.visit('https://automationteststore.com/');
+    cy.get('[href="https://automationteststore.com/index.php?rt=content/contact"]').click()
+  })
+  it('test happy path for contact us', () => {
+    cy.get('#ContactUsFrm_first_name').type('Joe');
+    cy.get('#ContactUsFrm_email').type('Joe@gmail.com');
+    cy.get('#ContactUsFrm_enquiry').type('Enquiry is entered here');
+    cy.get('button[type="submit"][title="Submit"]').click();
+    cy.contains(/continue/i).click();
+    cy.url().should('equal', 'https://automationteststore.com/')
+  })
+})
---
+++ cypress-folder/cypress/integration/webdrive-uni/contact.spec.ts
@@ -0,0 +1,23 @@
+/// <reference types="cypress">
+
+describe('Test Contact Us Page', () => {
+  beforeEach(() => {
+    cy.visit('http://webdriveruniversity.com/index.html');
+    cy.get('#contact-us').invoke('attr','target','_self').click();
+  })
+  it('should be able to submit a correctly filled up form', () => {
+    cy.get('[name="first_name"]').type('Joe');
+    cy.get('[name="last_name"]').type('biden');
+    cy.get('[name="email"]').type('joebiden@gmail.com');
+    cy.get('[name="message"]').type('this is a message');
+    cy.get('[type="submit"]').click();
+    cy.contains(/thank you/i);
+  })
+
+  it.only('should not be able to submit form successfully if form not filled in', () => {
+    cy.get('[name="first_name"]').type('Tom');
+    cy.get('[name="last_name"]').type('biden');
+    cy.get('[name="message"]').type('This is wrong');
+    cy.get('[type="submit"]').click();
+  })
+})
